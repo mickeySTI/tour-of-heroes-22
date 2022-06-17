1e435d725d7c8c12ab9b39a3a93d734f0d248690
MGarcia1120 <imickey216@gmail.com>
2022-06-16 23:24:03 -0400

    completed two way binding section

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Hero } from './../../models/hero.interface';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
